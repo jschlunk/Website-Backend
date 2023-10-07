fffe4367b3a3a10b55e63d6c9d1d893fc9fb2dae
jschlunk <93251498+jschlunk@users.noreply.github.com>
2023-10-07 17:13:48 +0200

    create database zelda

---
+++ src/main/resources/db/migration/V2__Datenbank.sql
@@ -0,0 +1,27 @@
+CREATE TABLE zelda(
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    rangliste_avg FLOAT
+);
+
+INSERT INTO zelda (id, name, rangliste_avg) VALUES
+    (1, 'Zelda 1', 16.5),
+    (2, 'Adventure of Link', 17.3),
+    (3, 'A Link to The Past', 5.5),
+    (4, 'Links Awakening', 11.6),
+    (5, 'Ocarina of Time', 2.6),
+    (6, 'Majoras Mask', 4.5),
+    (7, 'Oracle of Seasons', 13.6),
+    (8, 'Oracle of Ages', 14.1),
+    (9, 'Four Swords', 15.5),
+    (10, 'Wind Waker', 6.9),
+    (11, 'Four Swords Adventures', 12.5),
+    (12, 'Minish Cap', 8.2),
+    (13, 'Twilight Princess', 13.2),
+    (14, 'Phantom Hourglass', 14.8),
+    (15, 'Spirit Tracks', 12.5),
+    (16, 'Skyward Sword', 11.5),
+    (17, 'A Link between Worlds', 9.3),
+    (18, 'Triforce Heroes', 16.4),
+    (19, 'Breath of the Wild', 2.9),
+    (20, 'Tears of the Kingdom', 5.6);
